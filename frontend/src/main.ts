516601375d198dba0d4bee4e343fb40a8fb69a69
Kea Tossavainen <keamesi@Keas-MacBook-Pro.local>
2019-01-22 21:31:37 +0200

    .gitignore updates

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,15 @@ import 'hammerjs';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DemoMaterialModule} from './material.module';
+
+
 if (environment.production) {
   enableProdMode();
 }
